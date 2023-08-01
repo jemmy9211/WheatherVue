88212e98b270af1ab0a6b7135cd13fbd50ddc5bd
jemmy9211 <jemmy9211@gmail.com>
2023-08-02 02:38:15 +0800

    i commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'This is some text'
+    };
+  }
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style>
+
+</style>
