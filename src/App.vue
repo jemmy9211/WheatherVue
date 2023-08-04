9109a14697f435c9fd198bf0fe9784814046bc7b
jemmy9211 <jemmy9211@gmail.com>
2023-08-05 04:52:21 +0800

    api success

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+import axios from 'axios'
+const url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-373C6328-6BF2-41B3-BB3B-147802B82875';
 export default {
   data() {
     return {
-      message: 'Wheather App using Vue'
+      wdatas: [],
+      data:null
     };
+  },
+  beforeCreate(){
+    axios.get(url).then((res)=>{
+      console.log(res.data)
+      this.wdatas[0]=res.data.records.location[31]
+      this.wdatas[1]=res.data.records.location[111]
+      this.wdatas[2]=res.data.records.location[45]
+      this.wdatas[3]=res.data.records.location[24]
+      this.wdatas[4]=res.data.records.location[3]
+      console.log(this.wdatas)
+    })
   }
 };
 </script>
@@ -11,15 +25,16 @@ export default {
 <template>
   <nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand"><i class="bi bi-emoji-sunglasses-fill"></i>{{ message }}</a>
+      <a class="navbar-brand"><i class="bi bi-emoji-sunglasses-fill"></i> Wheather App using Vue</a>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </nav>
-  <weather-block/>
-  <weather-block/>
+  <weather-block v-for="x in wdatas" 
+    v-bind:city="x" 
+/>
 </template>
 
 <style>
--- src/components/weatherblock.vue
+++ src/components/weatherblock.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="container">
-      <h2>{{ name }}</h2>
-      <p>{{ message }}</p>
-    </div>
-  </template>
+  <div class="p-3 mb-2 bg-secondary text-white">
+      <h3 class="text-white">{{ city.locationName }}</h3>
+      <span class="badge bg-dark">目前觀測氣溫:</span> {{ city.weatherElement[3].elementValue }}
+      <span class="badge bg-dark">天氣描述:</span> {{ city.weatherElement[20].elementValue }}
+  </div>
+</template>
   
   <script>
+  var a;
   export default {
-    data() {
+    props:['city'],
+    data(){
       return {
-        name: 'Tainan',
-        message: 'Sunny'
+        data:null
       }
     }
+    
   };
   </script>
   
