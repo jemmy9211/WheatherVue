14a8070cd8645f856a901c7a3a9b3a83f1945b9a
jemmy9211 <jemmy9211@gmail.com>
2023-08-08 21:43:36 +0800

    no message

--- src/components/rader.vue
+++ src/components/rader.vue
@@ -2,6 +2,11 @@
 
 </script>
 <template>
+    <div class="btn-group btn-group-sm border border-dark border-3 text-white">
+        <router-link type="button" class="btn btn-outline-light" to="/">回首頁</router-link>
+        <router-link type="button" class="btn btn-outline-light" to="/rader">及時雷達回波圖</router-link>
+        <button type="button" class="btn btn-outline-light" onclick="javascript:location.href='https://jemmy9211.github.io/'">Jemmy website</button>
+    </div>
     <div class="overflow-auto">
         <h5><span class="badge bg-success">雷達整合回波圖-臺灣</span></h5>
         <img src="https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-A0058-003.png" class="img-thumbnail" alt="Cinque Terre">
