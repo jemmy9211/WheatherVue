db938c612b93c9fbff188bac1ed871e001903639
jemmy9211 <jemmy9211@gmail.com>
2023-09-19 03:59:26 +0800

    pwa deploy

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <router-view></router-view>
     <footer class="flex-shrink-0 py-4 bg-dark text-white-50">
         <div class="container text-center">
-            <small>Copyright &copy; jemmy9211 Website</small><br>
+            <small>Copyright &copy; 2023 jemmy9211 Website</small><br>
             <small>資料來自中華民國氣象局</small>
         </div>
     </footer>
--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,15 @@ export default defineConfig({
     plugins: [
       vue()
     ]
-  })
+})
+
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'WeatherVue',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+   }
+}
